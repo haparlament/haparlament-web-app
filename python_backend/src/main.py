bfbb4d0f6d1c8025d732970f4d30e6c429a867cf
david <davidvoler@gmail.com>
2023-03-31 15:11:46 +0300

    solving export

--- python_backend/src/main.py
+++ python_backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import users, sessions, images, moderators, session_request
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -9,3 +10,5 @@ app.include_router(users.router, prefix="/api/v2/users")
 app.include_router(images.router, prefix="/api/v2/images")
 app.include_router(sessions.router, prefix="/api/v2/sessions")
 app.include_router(session_request.router, prefix="/api/v2/session_request")
+
+app.mount("/", StaticFiles(directory="static"), name="static")
