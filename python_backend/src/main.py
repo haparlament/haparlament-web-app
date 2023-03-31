5e246868b1a613a682e59c35983f547ed4006d11
david <davidvoler@gmail.com>
2023-03-31 15:28:52 +0300

    removing index code

--- python_backend/src/main.py
+++ python_backend/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers import users, sessions, images, moderators, session_request
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -12,3 +12,10 @@ app.include_router(sessions.router, prefix="/api/v2/sessions")
 app.include_router(session_request.router, prefix="/api/v2/session_request")
 
 app.mount("/", StaticFiles(directory="static"), name="static")
+# @app.get("/")
+# def index():
+#     try:
+#         with open("static/index.html") as f:
+#             response =  HTMLResponse(f.read())
+#     except:
+#         response = "Error"
