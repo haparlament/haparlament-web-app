a765876484b4678f21b293d6ecbfceb66b485bae
Yotam Shalev <yotams@wix.com>
2023-04-09 19:37:32 +0300

    ui fixes - remove profile image and footer and fix intro texts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { letsTalk, whatDoYouThink } from "../styles.css/icons.svg/icons";
 import "../styles.css/Footer.css";
 
 function Footer() {
   return (
     <footer>
-      {whatDoYouThink}
-      {letsTalk}
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles.css/Header.css";
-import Profilepic from "../styles.css/images/profilepic.jpeg";
 
 function Header() {
   return (
     <header className="header-div">
       <h3 className="header">הפרלמנט</h3>
-      <img className="profile-pic" src={Profilepic} alt="" />
     </header>
   );
 }
