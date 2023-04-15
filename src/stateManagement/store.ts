f475f9172268ee26d8cd4f2acc7d9588533ac56e
Yotam Shalev <yotams@wix.com>
2023-04-15 17:08:41 +0300

    wip

--- src/stateManagement/store.ts
+++ src/stateManagement/store.ts
@@ -3,13 +3,10 @@ import sessionSubscriptionReducer from './modules/sessionSubscription/sessionSub
 
 export const store = configureStore({
   reducer: {
-    // posts: postsReducer,
-    // comments: commentsReducer,
     sessionSubscription: sessionSubscriptionReducer,
   },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
