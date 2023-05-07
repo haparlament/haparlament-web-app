0ffe7996b9312891f8e207adefc455c5f3f5c75b
Yotam Shalev <yotams@wix.com>
2023-05-07 20:30:49 +0300

    migrate airtable fields

--- functions/src/dto/session-request.ts
+++ functions/src/dto/session-request.ts
@@ -1,6 +1,4 @@
 import { IsNotEmpty, IsString, IsDate, IsMobilePhone, MaxLength } from 'class-validator';
-import { TimeRange } from '../controllers/session-request';
-
 /**
 * Validates create session DTO.
 */
@@ -11,7 +9,7 @@ export class CreateSessionRequestDto {
         feeling: string,
         phoneNumber: string,
         days: string[],
-        hourRanges: TimeRange[],
+        hourRanges: string[],
     ) {
         this.userName = userName;
         this.imageId = imageId;
@@ -47,7 +45,7 @@ export class CreateSessionRequestDto {
     @MaxLength(10, {
         each: true,
     })
-        hoursRanges: TimeRange[];
+        hoursRanges: string[];
 
     @IsNotEmpty()
     @IsDate()
@@ -77,7 +75,7 @@ export class CreateSessionRequestDto {
             Feeling: this.feeling,
             PhoneNumber: this.phoneNumber,
             Days: this.days,
-            HourRanges: this.hoursRanges,
+            AvailableHours: this.hoursRanges,
         };
     }
 }
--- src/utils/session_request.ts
+++ src/utils/session_request.ts
@@ -1,6 +1,6 @@
 import { SessionSubscriptionData } from '../stateManagement/modules/sessionSubscription/sessionSubscriptionSlice'
 const BASE_API_URL = 'https://us-central1-haparlament.cloudfunctions.net/api/v1';
-//const BASE_API_URL = 'http://127.0.0.1:5001/haparlament/us-central1/api/v1'; // TODO - use this when running locally
+// const BASE_API_URL = 'http://localhost:5001/haparlament/us-central1/api/v1'; // TODO - use this when running locally
 
 async function postData(url: string, data: string) {
     const response = await fetch(url, {
@@ -34,7 +34,7 @@ export function postSessionRequest(session: SessionSubscriptionData) {
       imageId: session.imageEmotion.imageId,
       emotion: session.imageEmotion.emotion,
       days: session.timeAvailability.days,
-      hoursRanges: session.timeAvailability.hoursRanges,
+      hoursRanges: session.timeAvailability.hoursRanges.map(range => `${range.from.hour}-${range.to.hour}`),
     }
     let json =  JSON.stringify(serverDto);
     return postData(`${BASE_API_URL}/session-request/`, json);
