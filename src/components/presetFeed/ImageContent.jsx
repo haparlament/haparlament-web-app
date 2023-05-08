8e664c85d7c690d06e1c30cba933f8239dd2e706
Yotam Shalev <yotams@wix.com>
2023-05-08 19:01:49 +0300

    replace images

--- src/components/presetFeed/ImageContent.jsx
+++ src/components/presetFeed/ImageContent.jsx
@@ -3,18 +3,23 @@ import Feelings from "../presetFeed/Feelings";
 import SubmitFeelings from "./SubmitFeelings";
 import { emotionSelectionImages } from "../../data/data";
 import "../../styles.css/ImageContent.css";
+import AYALON_PROTEST from "../../styles.css/images/ayalon-protest.jpg";
+import HAREDI_LEARNING from "../../styles.css/images/haredi-learning.jpg";
+import THE_SLAVE_MARCH from "../../styles.css/images/women-protest.jpg";
 
 function ImageContent({ slide, setSlide }) {
+  const images = {
+    AYALON_PROTEST: AYALON_PROTEST,
+    HAREDI_LEARNING: HAREDI_LEARNING,
+    THE_SLAVE_MARCH: THE_SLAVE_MARCH
+  }
   const [imageIndex, setImgIndex] = useState(0);
-  const [imgURL, setImgURL] = useState(emotionSelectionImages[imageIndex].url);
-
   const [imgID, setImgID] = useState(emotionSelectionImages[imageIndex].id);
   const [selectedEmotionId, setSelectedEmotionId] = useState(null);
 
   function setNextImage() {
     const nextImageIndex = imageIndex < emotionSelectionImages.length - 1 ? imageIndex + 1 : 0;
     setImgID(emotionSelectionImages[nextImageIndex].id);
-    setImgURL(emotionSelectionImages[nextImageIndex].url)
     setImgIndex(nextImageIndex)
   }
 
@@ -26,7 +31,7 @@ function ImageContent({ slide, setSlide }) {
   return (
     <div className="big-card">
       <div className="img-div">
-        {imgURL && <img src={imgURL} className="img-content" alt="" />}
+        {<img src={images[imgID]} className="img-content" alt="" />}
       </div>
       <Feelings
         selectedEmotionId={selectedEmotionId}
