12e06d573cd271c6215f58bd450354c47f8ecf87
yotamshalev <30769427+yotamshalev@users.noreply.github.com>
2023-05-09 12:48:15 +0300

    replace images (#39)

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,23 +1,14 @@
 export const emotionSelectionImages = [
   {
-    url: "https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392,f_auto/v1677351424/direct/WhatsApp_Image_2023-02-25_at_20.56.35_ybhpmv.jpg",
-    id: "KAPLAN_PROTEST",
-    description: "Kaplan Protest in Saturday night",
+    id: "AYALON_PROTEST",
+    description: "Ayalon Protest in Saturday night",
   },
   {
-    url: "https://pbs.twimg.com/media/EeQPtqKXYAYFRrj?format=jpg&name=900x900",
-    id: "POLICE_VIOLENCE",
-    description: "Police violance",
-  },
-  {
-    url: "https://ynet-pic1.yit.co.il/picserver5/crop_images/2023/02/13/S1eH5hvDpo/S1eH5hvDpo_0_25_1280_720_0_x-large.jpg",
-    id: "SIMCHA_ROTMAN",
-    description:
-      "Simcha Rothman, chairman of the Constitutional Law and Law Committee, argues with the committee's legal advisor",
-  },
-  {
-    url: "https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_750/811686",
     id: "THE_SLAVE_MARCH",
     description: "The slave march in protest of the legal revolution",
+  },
+  {
+    id: "HAREDI_LEARNING",
+    description: "A haredi man learning tora",
   },
 ];
