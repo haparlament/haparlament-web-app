6c5e26f76db0d35ca816c6a10c9b03fbf42c63ee
Itai Bieber <itaibieberworks@gmail.com>
2023-05-21 19:08:50 +0300

    Redesigned the success message Added placeholder success icon

--- src/components/pages/SessionSentMessage.tsx
+++ src/components/pages/SessionSentMessage.tsx
@@ -1,8 +1,12 @@
+import "../../styles.css/SessionSentMessage.scss"
+import successIcon from "../../styles.css/images/successIcon.png";
+
 function SessionSentMessage() {
 
   return (
     <div className="big-card details-form-div full-screen-mode">
-      <div className="details-form-headers">
+      <img className="success-icon" src={successIcon} alt="Success!" />
+      <div className="session-sent-message-headers">
         <h2>נרשמת!</h2>
         <h4>בקרוב ניצור איתך קשר בוואטסאפ ונקשר אותך לשיחה עם אדם עם תחושות שונות בנושא שבחרת</h4>
       </div>
