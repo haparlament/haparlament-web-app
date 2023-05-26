5135824fac01ff84b10257bacc0c68f9562da2bf
Yotam Shalev <yotams@wix.com>
2023-05-26 13:39:31 +0300

    send different messages to each participant

--- src/pages/VideoCall/components/Instructions.js
+++ src/pages/VideoCall/components/Instructions.js
@@ -1,80 +1,31 @@
-import React, { useCallback, useState, useEffect, useRef } from 'react';
-import {
-  useParticipantIds,
-} from '@daily-co/daily-react-hooks';
+import React, { useCallback, useState, useEffect, useRef } from "react";
+import { useParticipantIds, useLocalParticipant, useDaily } from "@daily-co/daily-react-hooks";
+import { getInstructionsPlayer } from "./../../../services/video/instructionsPlayer";
+import "./Instructions.css";
 
 
-import './Instructions.css';
-function InstructionsPlayer({ onInstructionChange }) {
-  let _callStarted = false;
-  let _time = 0;
-  let _interval = null;
-  const callInstructionsScript = {
-    1: {
-      text: 'Welcome to the meeting!',
-    },
-    5: {
-      text: 'Please introduce yourself!',
-    },
-    10: {
-      text: 'The topic of the meeting is...',
-    },
-    20: {
-      text: 'Participant 1, please state your opinion',
-    },
-    30: {
-      text: 'Participant 2, please state your opinion',
-    },
-    40: {
-      text: 'Meeting is over, thank you for your participation!',
-    }
-  }
-  return {
-    play: () => {
-      console.log('play');
-      if (!_callStarted) {
-        _callStarted = true;
-        _interval = setInterval(() => {
-          _time+=1;
-          console.log('tick', _time)
-          if (callInstructionsScript[_time]) {
-            console.log('script', callInstructionsScript[_time].text);
-            onInstructionChange(callInstructionsScript[_time].text)
-          }
-        }, 1000);
+// function getParticipantType() {
+//   if (participantId === localParticipantId) {
+//     return "local";
+//   }
+//   return "remote";
+// }
 
-      }
-    },
-    pause: () => {
-      // TODO allow to resume call instructions from the point where it was paused
-      console.log('pause');
-      if (_callStarted) {
-        _callStarted = false;
-        clearInterval(_interval);
-      }
-    },
-    // TODO add stop function
-    isPlayed: () => {
-      return _callStarted;
-    } 
-  }
-}
 export default function Instructions() {
-  const [instruction, setInstruction] = useState('');
-  let instructionsPlayer = useRef(new InstructionsPlayer({ onInstructionChange: setInstruction }));
-
-  const allParticipants = useParticipantIds();
-  console.log('allParticipants', allParticipants);
-  if (allParticipants.length > 1 && !instructionsPlayer.current.isPlayed()) {
-    instructionsPlayer.current.play();
+  // const allParticipants = useParticipantIds();
+  const localParticipant = useLocalParticipant();
+  const remoteParticipantIds = useParticipantIds({ filter: 'remote' });
+  const [instruction, setInstruction] = useState("");
+  // const [instructionsPlayer] = useState(() => getInstructionsPlayer(setInstruction));
+  const instructionsPlayerRef = useRef(getInstructionsPlayer())
+  if (remoteParticipantIds.length > 0 && !instructionsPlayerRef.current.isPlayed()) {
+    const otherParticipant = remoteParticipantIds[0];
+    const participant = localParticipant?.user_id > otherParticipant ? 'participantA' : 'participantB';
+    instructionsPlayerRef.current.play(participant, setInstruction);
   } else {
-    if (instructionsPlayer.current.isPlayed() && allParticipants.length < 2) {
-      instructionsPlayer.current.pause();
-    }
+    // if (instructionsPlayerRef.current.isPlayed() && allParticipants.length < 2) {
+    //   instructionsPlayerRef.current.pause();
+    // }
   }
-  return (
-    <div className="instructions">
-      {instruction}
-    </div>
-  );
+  return <div className="instructions">{instruction}</div>;
 }
